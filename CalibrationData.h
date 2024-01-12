0630f6f7e4c0a2498b512db257bf7a8dfc2aabe6
Matt Gaidica, Ph.D <matt@gaidi.ca>
2024-01-12 11:16:12 -0600

    Calibration data

--- CalibrationData.h
+++ CalibrationData.h
@@ -1,6 +1,6 @@
 #ifndef CalibrationData_h
 #define CalibrationData_h
 
-extern float calibrationData[8];  // Declaration
+extern float calibrationData[548][9];  // Declaration, get that number from MATLAB
 
 #endif
